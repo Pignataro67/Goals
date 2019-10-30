45a5bc900d58a14478f8a822a09017f279cbbfef
Pignataro67 <Pignataro67@gmail.com>
2019-10-30 04:45:04 -0700

    adding comp and cont

---
+++ client/src/containers/GoalsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AddGoal from '../components/Goals/AddGoal';
+
+
+class GoalsContainer extends Component {
+  render(){
+    return(
+      <div>
+        <AddGoal />
+      </div>
+    )
+  }
+}
+
+export default GoalsContainer;
