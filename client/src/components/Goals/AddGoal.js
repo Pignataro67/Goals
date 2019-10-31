29e8612f7effd2e88bd487ff7735e0438ab30edf
Pignataro67 <Pignataro67@gmail.com>
2019-10-31 12:35:18 -0700

    adding state

--- client/src/components/Goals/AddGoal.js
+++ client/src/components/Goals/AddGoal.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 export default class AddGoal extends Component {
+  
+  constructor(){
+    super()
+    this.state = {
+      value: ""
+    }
+  }
+
+  handleChange = (e) =>{
+    this.setState({
+      value: e.target.value
+    })
+
+  }
+  
   render(){
     return(
       <div  className="main-content">
         <h3>Anotha' Goal</h3>
         <form>
-          <input type="text" placeholder="What would you like to achieve" />
+          <input type="text" />
+            value={this.state.value} 
+            onChange={(e) => this.handleChange(e)}
+            placeholder="What would you like to achieve" />
+
+          {this.state.value}
           <input type="submit" value="Submit" />
         </form>
       </div>
