59da9bc0526194c9ea5ee7e1d9a033f3fa31bcdc
Pignataro67 <Pignataro67@gmail.com>
2019-10-31 10:27:41 -0700

    adding home comp and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import GoalsContainer from './containers/GoalsContainer';
 import NavBar from './components/Navbar';
 import { Route } from 'react-router-dom';
+import Home from './components/Home';
 import AddGoal from './components/Goals/AddGoal';
+import GoalsContainer from './containers/GoalsContainer';
 
 function App() {
   return (
     <div className="App">
         <h1>Goals</h1>
-        hello
+        <p>Stay Focused</p>
         <NavBar />
 
         <div>
+          <Route exact path='/' render={Home} />
           <Route exact path='/goals/new' component={AddGoal} />
           <Route exact path='/goals' component={GoalsContainer} />
         </div>
---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Home = () => (
+  <div>
+    Welcome Home
+  </div>
+)
+
+export default Home;
