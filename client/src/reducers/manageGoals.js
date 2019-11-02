36b07f499fe0e84eeae7e0267032ed5ad417bd8e
Pignataro67 <Pignataro67@gmail.com>
2019-11-02 06:09:34 -0700

    now rendering

--- client/src/reducers/manageGoals.js
+++ client/src/reducers/manageGoals.js
@@ -4,10 +4,7 @@ export default function goalsReducer(state = {
   switch(action.type){
     case 'ADD_GOAL':
       console.log('adding', action.goal )
-      return {
-        ...state,
-        goals: [...state.goals, action.goal]
-      }
+      return {...state, goals: [...state.goals, action.goal]}
     default:
       console.log("hit default case in reducer")
       return state;
