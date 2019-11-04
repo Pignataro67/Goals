361bb4c39293da474c4724b194b9cc9f05f2312a
Pignataro67 <Pignataro67@gmail.com>
2019-11-04 11:26:44 -0800

    improving reducer

--- client/src/reducers/manageGoals.js
+++ client/src/reducers/manageGoals.js
@@ -3,6 +3,12 @@ export default function goalsReducer(state = [], action) {
     case 'ADD_GOAL':
       console.log('adding', action.goal )
       return [...state, action.goal]
+    case 'LOADING_GOALS':
+      console.log("loading goals now")
+      return {...state, loading: [...state.loading, true]}
+    case 'FETCH_GOALS':
+      console.log("fetch goals")
+      return {loading: false, goals: action.payload}
     default:
       console.log("hit default case in reducer")
       return state;
