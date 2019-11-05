ff9c9efd50211a4dc5fa9f6d89d133e2acafa0d8
Pignataro67 <Pignataro67@gmail.com>
2019-11-05 09:15:40 -0800

    crud-ing obj controller, last commit was goal controller

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,2 +1,30 @@
 class ObjectivesController < ApplicationController
+  def index
+    @objectives = Objective.all
+    render json: @objectives
+  end
+
+  def show
+    @objective = Objective.find(params[:id])
+    render json: @objective, status: 200
+  end
+
+  def create
+    @objective = Objective.create(objective_params)
+    render json: @objective, status: :created
+  end
+
+  def destroy
+    @objective = Objective.find(params[:id])
+    if @objective.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+  def objective_params
+    params.require(:objective).permit(:aim, :strategy, :category)
+  end
 end
