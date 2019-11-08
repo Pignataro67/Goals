fa55d9eac80a15b0d6cc800d83eb3521f535b7f2
Pignataro67 <Pignataro67@gmail.com>
2019-11-08 11:45:36 -0800

    adding state and to return in obj comp.

--- client/src/components/Objectives/ObjectiveInput.js
+++ client/src/components/Objectives/ObjectiveInput.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Button, Form } from 'semantic-ui-react'
+import { addObjective } from '../../actions/objectivesActions'
 
-export default class ObjectiveInput extends Component {
+class ObjectiveInput extends Component {
+  constructor(){
+    super()
+    this.state = {
+      description: ""
+    }
+  }
+
+  handleChange = (e) =>{
+    this.setState({
+      [e.target.id] : e.target.value
+    })
+  }
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    this.props.addObjective(this.state, this.props.goalId);
+    this.setState({
+      description: ''
+    })
+  }
+
   render(){
     return(
-      <div>Objective Form</div>
+        <Form className="new-objective-form" onSubmit={(e) =>this.handleOnSubmit(e)}>
+
+        <Form.Field>
+          <label>Add Objective</label>
+          <input placeholder="Define the milestones to help you reach your goal" id="description" required value={this.state.description} onChange={(e) =>this.handleChange(e)} />
+        </Form.Field>
+
+        <Button size='mini' type="submit">Submit</Button>
+      </Form>
     )
   }
 }
+
+export default connect(null, { addObjective })(ObjectiveInput);
