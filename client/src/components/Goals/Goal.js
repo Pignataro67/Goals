def21ff6d0381416fc76bef1f826e25b88af0a29
Pignataro67 <Pignataro67@gmail.com>
2019-11-11 10:06:14 -0800

    changing to dispatch to props

--- client/src/components/Goals/Goal.js
+++ client/src/components/Goals/Goal.js
@@ -17,7 +17,7 @@ class Goal extends Component {
         <strong>Category:</strong>
             <p>{category}</p>
         <Button basic color='red' size='mini' onClick={() => this.props.deleteGoal(this.props.id)}>Delete</Button>
-        <p><ObjectivesContainer goal={goal} /></p>
+        <p><ObjectivesContainer goal={goal} objectives={objectives}/></p>
       </div>
     )
   }
