8d50ce087533d2b084ef793d072be64e308ca4f9
Pignataro67 <Pignataro67@gmail.com>
2019-11-18 09:59:04 -0800

    adding md2p back to obj cont

--- client/src/containers/ObjectivesContainer.js
+++ client/src/containers/ObjectivesContainer.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import Objectives from '../components/Objectives/Objectives';
 import ObjectiveInput from '../components/Objectives/ObjectiveInput';
+import { addObjective, deleteObjective, fetchGoalObjectives } from '../actions/objectivesActions';
+import { connect } from 'react-redux';
 
 class ObjectivesContainer extends Component {
   
   render(){
+    debugger
     return(
       <div>
         Objectives Container
-        <Objectives objectives={this.props.objectives} deleteObjective={this.props.deleteObjective}  />
-        <ObjectiveInput addObjective={this.props.addObjective} goalId={this.props.goal.id} />
+        {/* <Objectives objectives={this.props.objectives} deleteObjective={this.props.deleteObjective}  />
+        <ObjectiveInput addObjective={this.props.addObjective} goalId={this.props.goal.id} /> */}
       </div>
     )
   }
 }
 
-// const mapStateToProps = (state) => {
-//   return {objectives: state.objectives.objectivesData}
-// }
+const mapStateToProps = (state) => {
+  return {objectives: state.objectives.objectivesData}
+}
 
-// const mapDispatchToProps = dispatch => ({
-//   addObjective: (objectiveInput, goalId) => dispatch(addObjective(objectiveInput, goalId)),
-//   deleteObjective: objectiveId => dispatch(deleteObjective(objectiveId))
-// })
+const mapDispatchToProps = dispatch => ({
+  addObjective: (objectiveInput, goalId) => dispatch(addObjective(objectiveInput, goalId)),
+  deleteObjective: objectiveId => dispatch(deleteObjective(objectiveId))
+})
 
-export default ObjectivesContainer;
+export default connect(null, mapDispatchToProps)(ObjectivesContainer);
