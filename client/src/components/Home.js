0d1d6909f69e0e51fa559ffd525e1fb021091043
Pignataro67 <Pignataro67@gmail.com>
2019-11-30 06:20:04 -0800

    stylin', spacen'

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Home = () => (
   <div  className="home-page">
     <h4>Time's ticking, what's next on your <a href="/goals">list?</a></h4>
+    <article>The benefits of Goal Tracking:</article>
   </div>
 )
 
--- client/src/components/Objectives/Objective.js
+++ client/src/components/Objectives/Objective.js
@@ -3,32 +3,14 @@ import { Checkbox, Icon } from 'semantic-ui-react';
 
 export default class Objective extends Component {
 
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     completed: false
-  //   }
-  // }
-
-  // handleCheck = e => {
-  //   e.preventDefault();
-  //   if (this.state.completed === false){
-  //     this.setState({completed: true})
-  //   } else {
-  //     this.setState({completed: false})
-  //   }
   handleCheck = () => {
     this.props.toggleCompleted(this.props.id)
-  }
-
-  handleClick = e => {
-    e.preventDefault();
-    alert('clicked delete Icon!')
   }
 
   render(){
     return(
       <div>
+        <Icon className="pointer" name='edit' color='blue' onClick={() => console.log("edit some shhhhhh")} />
         change delete button to icon
         <Icon className="pointer" name='delete' color='red' onClick={() => this.props.deleteObjective(this.props.id)} />
         <Checkbox value={this.props.completed} checked={!!this.props.completed} onChange={(e) => this.handleCheck(e)} />
