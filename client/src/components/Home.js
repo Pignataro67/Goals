2b44d88b124f9654546654e24a9a5b743e3e5167
Pignataro67 <Pignataro67@gmail.com>
2020-04-13 06:43:22 -0700

    some stylin', spacin', titlin'

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const Home = () => (
   <div  className="home-page">
     <h4>Time's ticking, what's next on your <Link to="/goals">list?</Link></h4>
-    <article>The benefits of Goal Tracking:</article>
   </div>
 )
 
