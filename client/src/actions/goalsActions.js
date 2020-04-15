1b2a79c2d4e7e5845dce9122a01f0dcd3e687f04
Pignataro67 <Pignataro67@gmail.com>
2020-04-15 10:10:22 -0700

    improving func.

--- client/src/actions/goalsActions.js
+++ client/src/actions/goalsActions.js
@@ -1,24 +1,20 @@
 const goalsURL = "/api/goals";
-// const headers = {
-//   'Accept': 'application/json',
-//   'Content-Type': 'application/json'
-// }
 
 export const fetchGoals = () => {
   return (dispatch) => {
-    dispatch({type: "LOADING_GOALS"})
+    dispatch({type: 'LOADING_GOALS'})
       return fetch(goalsURL)
         .then(response => response.json())
         .then(goals => dispatch({type: 'FETCH_GOALS', payload: goals}))
   }
 }
 
-export function fetchGoal(id){
+export const fetchGoal = id => {
   return (dispatch) => {
-    dispatch({type: "LOADING_GOALS"})
+    dispatch({type: 'LOADING_GOALS'})
       return fetch(`${goalsURL}/${id}`)
         .then(response => response.json())
-        .then(goal => dispatch({type: "FETCH_GOAL", payload: goal}))
+        .then(goal => dispatch({type: 'FETCH_GOAL', payload: goal}))
   }
 }
 
@@ -31,7 +27,6 @@ export const addGoal = goalInput => {
     },
     body: JSON.stringify(goalInput)
   }
-    
   return dispatch => {
     fetch(`${ goalsURL }`, data)
       .then(response => response.json())
@@ -51,7 +46,6 @@ export const deleteGoal = goal_id => {
       'Content-Type': 'application/json'
     }
   }
-  
   return dispatch => {
     fetch(`${goalsURL}/${goal_id}`, data)
       .then(response => response.json())
