94c0cd66ee92dcc61de20d3fa446e21565bc35cb
Pignataro67 <Pignataro67@gmail.com>
2020-04-21 09:33:10 -0700

    improving fallback routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   end
 
   get '/api/goals/:id/toggle', to: 'goals#toggle'
-  resources :objectives, only: :destroy
+  resources :objectives, only: [:destroy]
+
   patch '/api/objectives/:id', to: 'objectives#toggle'
+  // Fallback
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
